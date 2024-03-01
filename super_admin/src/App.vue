76ab1dd1b02ac09b50382c9000a5f976448afd4e
MAXIM18\18kmv <18kmv@80gmail.com>
2024-03-01 12:41:16 +0300

    #009-feature: added menu minimization

--- super_admin/src/App.vue
+++ super_admin/src/App.vue
@@ -1,19 +1,32 @@
 <template>
-  <AppHeaderComponent title="BaSys: DB manage"></AppHeaderComponent>
-  <div class="grid h-screen" style="margin-top:0">
-    <div class="bs-nav-panel col-fixed" style="padding:0">
+  <AppHeaderComponent
+    title="BaSys: DB manage"
+    @burgerClicked="onBurgerClicked">
+  </AppHeaderComponent>
+  <div class="grid h-screen" style="margin:0">
+    <div class="bs-nav-panel col-fixed" style="padding:0" :style="{'width': navPanelWidth + 'px'}">
       <nav>
-        <Menu :model="menuItems" style="min-width: 0;">
+        <Menu :model="menuItems" style="min-width: 0; border: 0;">
           <template #item="{ item, props }">
             <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
               <a v-ripple :href="href" v-bind="props.action" @click="navigate">
-                <span :class="item.icon" />
-                <span class="ml-2">{{ item.label }}</span>
+                <div v-if="isMenuMinimized">
+                  <span :class="item.icon" v-tooltip.right="item.label" />
+                </div>
+                <div v-else>
+                  <span :class="item.icon" />
+                  <span class="ml-2">{{ item.label }}</span>
+                </div>
               </a>
             </router-link>
             <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
-              <span :class="item.icon" />
-              <span class="ml-2">{{ item.label }}</span>
+              <div v-if="isMenuMinimized">
+                <span :class="item.icon" v-tooltip.right="item.label" />
+              </div>
+              <div v-else>
+                <span :class="item.icon" />
+                <span class="ml-2">{{ item.label }}</span>
+              </div>
             </a>
           </template>
         </Menu>
@@ -38,6 +51,9 @@ import AppHeaderComponent from './components/AppHeaderComponent.vue';
   },
 })
 export default class App extends Vue {
+  isMenuMinimized = false;
+  navPanelWidth = 200;
+
   menuItems: any = ref([
     { label: 'Home', icon: 'pi pi-home', route: '/' },
     { separator: true },
@@ -45,8 +61,18 @@ export default class App extends Vue {
     { label: 'Databases', icon: 'pi pi-database', route: '/databases' },
     { separator: true },
     { label: 'Logout', icon: 'pi pi-sign-out' },
-
+    { separator: true },
   ]);
+
+  onBurgerClicked(): void {
+    this.isMenuMinimized = !this.isMenuMinimized;
+
+    if (this.isMenuMinimized) {
+      this.navPanelWidth = 50;
+    } else {
+      this.navPanelWidth = 200;
+    }
+  }
 }
 </script>
 
@@ -65,7 +91,6 @@ body {
 }
 
 .bs-nav-panel {
-  width: 200px;
   border-right: 1px solid var(--surface-200);
 }
 </style>
--- super_admin/src/components/AppHeaderComponent.vue
+++ super_admin/src/components/AppHeaderComponent.vue
@@ -1,23 +1,42 @@
 <template>
     <div class="grid bs-app-header">
-        <div class="col-fixed" style="width:30px"></div>
+        <div
+            class="col-fixed flex justify-content-center align-content-center flex-wrap"
+            style="width:50px"
+        >
+            <span
+                class="pi pi-bars"
+                style="font-size: 1.5rem; cursor: pointer;"
+                :onclick="burgerClick">
+            </span>
+        </div>
         <div class="col">
             <span> {{ title }}</span>
         </div>
-
     </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import Button from 'primevue/button';
 
 @Options({
   props: {
     title: String,
   },
+  components: {
+    Button,
+  },
+  emits: {
+    burgerClicked: null,
+  },
 })
 export default class AppHeader extends Vue {
     title!:string;
+
+    burgerClick(): void {
+      this.$emit('burgerClicked');
+    }
 }
 
 </script>
