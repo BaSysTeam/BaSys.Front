8d0e8ea74496a22582e1e698c5c942a2b00547bb
ZyuDev <MikhaZyu@gmail.com>
2024-03-01 13:39:44 +0300

    #015-feature: publish settings

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -6,8 +7,12 @@ module.exports = defineConfig({
     extract: false,
   },
   configureWebpack: {
+    output: {
+      filename: 'admin.js',
+    },
     optimization: {
       splitChunks: false,
     },
   },
+  outputDir: path.resolve(__dirname, '../../BaSys.Back/src/BaSys.Host/wwwroot/app/admin'),
 });
