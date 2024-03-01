f2fcb1a5026331f1446448d0baabdf2fb163b5f8
ZyuDev <MikhaZyu@gmail.com>
2024-03-01 12:19:44 +0300

    #015-feature: add admin and shared project

---
+++ admin/src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import App from './App.vue';
+import router from './router';
+
+// Importing PrimeVue styles
+import 'primevue/resources/themes/lara-light-blue/theme.css'; // theme
+import 'primevue/resources/primevue.min.css'; // core css
+import 'primeicons/primeicons.css'; // icons
+import 'primeflex/primeflex.css';
+
+createApp(App).use(PrimeVue).use(router).mount('#app');
