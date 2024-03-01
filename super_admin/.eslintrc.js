ccc839c9b453d555d214e986474f196ce3ca9ffe
ZyuDev <MikhaZyu@gmail.com>
2024-03-01 10:34:08 +0300

    Linter settings changed

--- super_admin/.eslintrc.js
+++ super_admin/.eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
       'always',
       { exceptAfterSingleLine: true },
     ],
-    'import/no-extraneous-dependencies': 'off',
+    //'import/no-extraneous-dependencies': 'off',
   },
   overrides: [
     {
