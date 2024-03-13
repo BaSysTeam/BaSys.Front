59b7317b7afe0526f5bef21cad3654703e8e7a25
ZyuDev <MikhaZyu@gmail.com>
2024-03-13 13:28:19 +0300

    Constructor front project

---
+++ constructor/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'no-shadow': 'off', // Turn off the base rule,
+    '@typescript-eslint/no-explicit-any': 'off',
+    'linebreak-style': ['off', 'windows'],
+    '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
+    'lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
+    'no-param-reassign': ['error', { props: false }],
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
---
+++ constructor/vue.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  css: {
+    extract: false,
+  },
+  configureWebpack: {
+    output: {
+      filename: 'constructor.js',
+    },
+    optimization: {
+      splitChunks: false,
+    },
+    resolve: {
+      alias: {
+        vue: path.resolve('./node_modules/vue'),
+      },
+    },
+  },
+  outputDir: path.resolve(__dirname, '../../BaSys.Back/src/BaSys.Host/wwwroot/app/constructor'),
+  devServer: {
+    proxy: 'https://localhost:7053',
+  },
+});
