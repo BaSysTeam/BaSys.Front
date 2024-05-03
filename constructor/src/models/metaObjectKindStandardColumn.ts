cc559e406460241b31aba40957db03a59fbf7dae
ZyuDev <MikhaZyu@gmail.com>
2024-05-03 16:38:08 +0300

    #159-feature: Add fields to standard column

--- constructor/src/models/metaObjectKindStandardColumn.ts
+++ constructor/src/models/metaObjectKindStandardColumn.ts
@@ -5,7 +5,11 @@ export default class MetaObjectKindStandardColumn {
     name:string;
     title: string;
     dataTypeUid:string;
+    stringLength:number;
+    numberDigits:number;
     isPrimaryKey:boolean;
+    isRequired:boolean;
+    isUnique:boolean;
     memo:string;
 
     constructor(params: any = {}) {
@@ -17,7 +21,11 @@ export default class MetaObjectKindStandardColumn {
       this.name = initialData.name || '';
       this.title = initialData.title || '';
       this.dataTypeUid = initialData.dataTypeUid || '';
+      this.stringLength = initialData.stringLength || 100;
+      this.numberDigits = initialData.numberDigits || 2;
       this.isPrimaryKey = initialData.isPrimaryKey || false;
+      this.isRequired = initialData.isRequired || false;
+      this.isUnique = initialData.isUnique || false;
       this.memo = initialData.memo || '';
     }
 }
