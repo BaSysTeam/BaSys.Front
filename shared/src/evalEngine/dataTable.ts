3ae058b288828d4a5d6bd130c5744b426c4f0bad
ZyuDev <MikhaZyu@gmail.com>
2024-09-05 12:16:26 +0300

    #208-feature: Group by in DataTable.

--- shared/src/evalEngine/dataTable.ts
+++ shared/src/evalEngine/dataTable.ts
@@ -156,6 +156,83 @@ export default class DataTable {
     return this;
   }
 
+  groupBy(keyColumns: string[], groupingColumns: any[]): DataTable {
+    const groupedTable = new DataTable();
+    const sourceKeyColumns: DataTableColumn[] = [];
+    const sourceGroupingColumns: DataTableColumn[] = [];
+
+    // Find key columns.
+    keyColumns.forEach((columnName: string) => {
+      const column = this.getColumn(columnName);
+
+      if (!column) {
+        throw new Error(`Column ${columnName} not found`);
+      }
+      sourceKeyColumns.push(column);
+      groupedTable.addColumn(column);
+    });
+
+    // Create key column.
+    const keyColumnName = '__key';
+    this.addColumn({ name: keyColumnName });
+
+    // Find grouping columns.
+    groupingColumns.forEach((columnName: any) => {
+      const column = this.getColumn(columnName);
+      if (!column) {
+        throw new Error(`Column ${columnName} not found`);
+      }
+
+      sourceGroupingColumns.push(column);
+      groupedTable.addColumn(column);
+    });
+
+    // Collect unique combinations of key columns.
+    const keyMap = new Map<string, any>();
+
+    this._rows.forEach((row: any) => {
+      const keyObject:any = {};
+      let key = '';
+      sourceKeyColumns.forEach((sourceColumn: any) => {
+        keyObject[sourceColumn.name] = row[sourceColumn.name];
+        key += `${sourceColumn.name}:${row[sourceColumn.name]}|`;
+      });
+      if (!keyMap.has(key)) {
+        keyMap.set(key, keyObject);
+      }
+      row[keyColumnName] = key;
+    });
+
+    // Filter rows by key columns.
+    keyMap.forEach((keyObject: any, key:string) => {
+      console.log('iterate over map');
+      console.log('key', key);
+      console.log('keyObject', keyObject);
+      const filterResult = this._rows.filter((row: any) => row[keyColumnName] === key);
+      const totals: any = {};
+
+      sourceGroupingColumns.forEach((sourceColumn: any) => {
+        totals[sourceColumn.name] = sourceColumn.defaultValue;
+      });
+
+      filterResult.forEach((row: any) => {
+        sourceGroupingColumns.forEach((sourceColumn: any) => {
+          totals[sourceColumn.name] += row[sourceColumn.name];
+        });
+      });
+
+      const newRow = groupedTable.newRow();
+      Object.entries(keyObject).forEach(([key, value]) => {
+        newRow[key] = value;
+      });
+      Object.entries(totals).forEach(([key, value]) => {
+        newRow[key] = value;
+      });
+    });
+
+    return groupedTable;
+  }
+
   private fillRowFromArray(data: any[]): void {
     const newRow = this.newRow();
     this._columns.forEach((column: any, index: number) => {
