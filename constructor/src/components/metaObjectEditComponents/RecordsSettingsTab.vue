ce2d64b30603511324f8224365045abc682c41a6
ZyuDev <mikhazyu@gmail.com>
2024-11-25 22:00:19 +0300

    #223-feature: Records settings tab for MetaObject.

---
+++ constructor/src/components/metaObjectEditComponents/RecordsSettingsTab.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {
+  ref, onMounted, onBeforeMount, defineProps, defineEmits, watch, computed, PropType,
+} from 'vue';
+import { useRouter } from 'vue-router';
+import { useToast } from 'primevue/usetoast';
+import { useConfirm } from 'primevue/useconfirm';
+import { useI18n } from 'vue-i18n';
+import MetaObjectStorableSettings from '../../../../shared/src/models/metaObjectStorableSettings';
+
+// Infrastructure
+const { t } = useI18n({ useScope: 'global' });
+const router = useRouter();
+const confirmVue = useConfirm();
+
+// Props
+const props = defineProps({
+  settings: {
+    type: Object as PropType<MetaObjectStorableSettings>,
+    required: true,
+  },
+});
+
+// Data
+
+// Emits
+const emit = defineEmits({ change: () => true });
+
+// Methods
+
+// Event handlers
+
+// Life cycle hooks
+</script>
+
+<template>
+<div>Record settings</div>
+</template>
+
+<style scoped>
+
+</style>
